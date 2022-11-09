2d11cd090b9efa933fc9861bb3598805ce22046b
azaz111 <86608104+azaz111@users.noreply.github.com>
2022-11-09 13:15:38 +0300

    Update gdrive_respons.py

--- gdrive_respons.py
+++ gdrive_respons.py
@@ -14,26 +14,26 @@ def service_avtoriz_v3(token='token.json'):# АВТОРИЗАЦИЯ  Drive API v
     'https://www.googleapis.com/auth/iam',
     'https://www.googleapis.com/auth/cloudplatformprojects'
     ]
-    #creds = None
+    creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
+    if os.path.exists(token):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+        with open(token, 'w') as tokenw:
+            tokenw.write(creds.to_json())
 
     service = build('drive', 'v3', credentials=creds)
     return service
+
 
 def new_drive_and_json( name:str , json:str ): # Создаем новый диск подключаем джисоны вход : желаемое имя  выход data drive
     service=service_avtoriz_v3()
