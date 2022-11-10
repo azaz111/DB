998dad563a247f1e2078b240e5b9c0dad8bb70de
azaz111 <86608104+azaz111@users.noreply.github.com>
2022-11-10 19:11:04 +0300

    Update gdrive_respons.py

--- gdrive_respons.py
+++ gdrive_respons.py
@@ -1,47 +1,28 @@
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from googleapiclient.errors import HttpError
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-import time , os
+import time
 from random import randint
 from json import loads
 
 def service_avtoriz_v3(token='token.json'):# АВТОРИЗАЦИЯ  Drive API v3  
     SCOPES = [
-    'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/cloud-platform',
-    'https://www.googleapis.com/auth/iam'
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/iam',
     ]
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(token):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(token, 'w') as tokenw:
-            tokenw.write(creds.to_json())
-
+    creds = Credentials.from_authorized_user_file(token, SCOPES)
     service = build('drive', 'v3', credentials=creds)
     return service
 
-
-def new_drive_and_json( name:str , json:str ): # Создаем новый диск подключаем джисоны вход : желаемое имя  выход data drive
-    service=service_avtoriz_v3()
+def new_drive_and_json( name:str , json:str , service=service_avtoriz_v3()): # Создаем новый диск подключаем джисоны вход : желаемое имя  выход data drive
     new_grive=None
     email=loads(open(json, 'r').read())['client_email']
     for x in range(5):
         try:
             if not new_grive:
                 new_grive = service.teamdrives().create(requestId=randint(1,9999999), body={"name":f"perenos_{name}"}).execute() #создать диск
+            
             service.permissions().create(fileId=new_grive['id'], 
                                          fields='emailAddress', 
                                          supportsAllDrives=True, 
@@ -56,8 +37,7 @@ def new_drive_and_json( name:str , json:str ): # Создаем новый ди�
             print(f'[ERROR Create Drive] {err}' )
             time.sleep(2)
 
-def move_one_file_round(new_file_l,id_foldnazna):  # Перенос  файла в указанную папрку или диск вход : Список айди которые нужно перенести и айди родителя     
-    service=service_avtoriz_v3()
+def move_one_file_round(new_file_l,id_foldnazna,service=service_avtoriz_v3()):  # Перенос  файла в указанную папрку или диск вход : Список айди которые нужно перенести и айди родителя     
     file=None
     for x in range(20):
         try:
@@ -75,8 +55,23 @@ def move_one_file_round(new_file_l,id_foldnazna):  # Перенос  файла 
             time.sleep(2)
     return False
 
-def delete_drive(s_iddrive):  # Перенос  файла в указанную папрку или диск вход : Список айди которые нужно перенести и айди родителя     
-    service=service_avtoriz_v3()
+def move_list_file_round(new_file_l,id_foldnazna,service=service_avtoriz_v3()):  # Перенос  файла в указанную папрку или диск вход : Список айди которые нужно перенести и айди родителя     
+    try:
+        for new_file in new_file_l:
+            file = service.files().get(fileId=new_file, supportsAllDrives=True, fields='parents').execute()
+            previous_parents = ",".join(file.get('parents'))
+            print(f'perenos :{new_file}')
+            file = service.files().update(fileId=new_file,
+                                          addParents=id_foldnazna,
+                                          supportsAllDrives=True, 
+                                          removeParents=previous_parents, fields='id, parents').execute()# перемещаем в бекапную папку
+        return True
+    except HttpError as err: 
+        print(f'[ERROR MOVE] Будем менять Диск Oшибка: {err}' )
+        time.sleep(2)
+        return False
+
+def delete_drive(s_iddrive,service=service_avtoriz_v3()):  # Перенос  файла в указанную папрку или диск вход : Список айди которые нужно перенести и айди родителя     
     for x in range(5):
         try:
             service.drives().delete(driveId=s_iddrive).execute()
