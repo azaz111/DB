5931b0d588a531c29a849f8ad754879769f0825c
azaz111 <86608104+azaz111@users.noreply.github.com>
2022-11-11 03:44:45 +0300

    Update gdrive_respons.py

--- gdrive_respons.py
+++ gdrive_respons.py
@@ -1,17 +1,36 @@
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 import time
+import os
 from random import randint
 from json import loads
 
+SCOPES = ["https://www.googleapis.com/auth/drive",
+          "https://www.googleapis.com/auth/cloud-platform",
+          "https://www.googleapis.com/auth/iam"]
+
 def service_avtoriz_v3(token='token.json'):# АВТОРИЗАЦИЯ  Drive API v3  
-    SCOPES = [
-    'https://www.googleapis.com/auth/cloud-platform',
-    'https://www.googleapis.com/auth/drive',
-    'https://www.googleapis.com/auth/iam',
-    ]
-    creds = Credentials.from_authorized_user_file(token, SCOPES)
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
     service = build('drive', 'v3', credentials=creds)
     return service
 
